ba6b7509a13eb7c6639f9bab246d38b73573933f
MarkOtwane <otwanemark254@gmail.com>
2025-08-30 19:20:32 +0300

    feat: binary to decimal using bitwise function or method

---
+++ Week 1/binary to decimal/bitwise operator.py
@@ -0,0 +1,14 @@
+def decToBinary(n):
+    bin = ""
+
+    while n>0:
+
+        bit = n & 1
+
+        bin += str(bit)
+        n = n>>1
+    return bin[::-1]
+    
+if __name__ == "__main__":
+    n =12
+    print(decToBinary(n))
