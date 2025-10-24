3cd8cde97db1747ef0b1342817fec1b41a7dcb5b
MarkOtwane <otwanemark254@gmail.com>
2025-10-24 17:10:00 +0300

    Basic disctionary in python

---
+++ Week 1/Training/dictionary.py
@@ -0,0 +1,18 @@
+# # my_dictionary = {
+#     'key': 'Value'
+# }
+# print(my_dictionary)
+person = {
+    "name": "John",
+    "age": 30,
+    "city": "Nairobi"
+}
+
+# print(person["name"])       # Output: John
+# print(person.get("age"))    # Output: 30
+
+# add something in a dictionary
+person['course'] = "Computer Science"
+person['age'] = "23"
+
+print(person)
---
+++ Week 1/Training/loops.py
@@ -0,0 +1,17 @@
+for number in range(11):
+    if number == 7:
+        continue #skip 7
+    print(number)
+
+name = "Mark"
+students = ["Mark", "John"]
+if name == "Mark":
+    print(f"{name}, Congrats")
+
+    # creating a list 
+    #listName = []
+    # accessing item in a list
+    # print(listName[2])
+
+    # adding items in a list(append(), insert)
+    #removing items from an existing list(pop(), del)
